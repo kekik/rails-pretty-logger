bd6805b550124e741652853031de04c88b66b08f
Mehmet <mehmetcelik4@gmail.com>
2018-10-26 09:16:23 +0300

    add log_file controller spec & set no database for rspec

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -14,6 +14,13 @@ module Rails
           end
         end
 
+        describe "GET #log_file" do
+          it "returns http success" do
+            get :log_file, params: {log_file: "development", date_range: {start: Time.now.strftime("%Y-%m-%d") , end: Time.now.strftime("%Y-%m-%d") }}
+            expect(response).to have_http_status(:success)
+          end
+        end
+
       end
 
     end
