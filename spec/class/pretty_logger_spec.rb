819a5297cd7b34daf7fea258329df5dc7251ec91
Mehmet <mehmetcelik4@gmail.com>
2018-10-30 10:07:13 +0300

    add more specs to logger

--- spec/class/pretty_logger_spec.rb
+++ spec/class/pretty_logger_spec.rb
@@ -10,8 +10,8 @@ module Rails
           before do
             log = 'Started GET "/rails-pretty-logger/dashboards" for 127.0.0.1 at ' + Time.now.strftime("%Y-%m-%d").to_s + ' 11:17:00 +0300
             Processing by Rails::Pretty::Logger::DashboardsController#index as HTML
-            Rendering /home/mehmet/project/rails/dum/rails-pretty-logger/app/views/rails/pretty/logger/dashboards/index.html.erb within layouts/rails/pretty/logger/application
-            Rendered /home/mehmet/project/rails/dum/rails-pretty-logger/app/views/rails/pretty/logger/dashboards/index.html.erb within layouts/rails/pretty/logger/application (3.3ms)
+            Rendering /home/project/rails/dum/rails-pretty-logger/app/views/rails/pretty/logger/dashboards/index.html.erb within layouts/rails/pretty/logger/application
+            Rendered /home/project/rails/dum/rails-pretty-logger/app/views/rails/pretty/logger/dashboards/index.html.erb within layouts/rails/pretty/logger/application (3.3ms)
             Completed 200 OK in 236ms (Views: 233.7ms | ActiveRecord: 0.0ms)'
             file_path = File.join(Rails.root, 'log', "rspec_test.log")
             File.open(file_path,"w") {|f| f.write(log) }
@@ -28,16 +28,21 @@ module Rails
           it "validation fails" do
             params = ActionController::Parameters.new(date_range: {"end" => (Time.now - 1.days).strftime("%Y-%m-%d"),
               "start" => Time.now.strftime("%Y-%m-%d")}, log_file: "rspec_test" )
-              subject = PrettyLogger.new( params )
-              expect(subject.error).to eq("End Date should not be less than Start Date.")
-              expect(subject.logs_count).to eq(0)
-            end
-
-            after do
-              file_path = File.join(Rails.root, 'log', "rspec_test.log")
-              File.delete(file_path)
-            end
+            subject = PrettyLogger.new( params )
+            expect(subject.error).to eq("End Date should not be less than Start Date.")
+            expect(subject.logs_count).to eq(0)
           end
+
+          it "get log file list" do
+            log_file_count = PrettyLogger.get_log_file_list.count
+            expect(log_file_count).to be >= 1
+          end
+
+          after do
+            file_path = File.join(Rails.root, 'log', "rspec_test.log")
+            File.delete(file_path)
+          end
+        end
 
         end
       end
