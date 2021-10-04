5802f0dcc7f596154a43142091339229c35d7739
ulsyemr <emreulusoy@bitbucket.org>
2021-10-04 10:36:33 +0300

    Refactored application helper.

--- app/helpers/rails/pretty/logger/application_helper.rb
+++ app/helpers/rails/pretty/logger/application_helper.rb
@@ -1,17 +1,13 @@
-module Rails
-  module Pretty
-    module Logger
-      module ApplicationHelper
+module Rails::Pretty::Logger
+  module ApplicationHelper
 
-        def modify_name(name)
-          return "#{name [-13..-10]}/#{name[-9..-8]}/#{name[-7..-6]} : #{name[-4..-1]}"
-        end
-
-        def trim_name(name)
-          index = name.split("/log/").last.capitalize
-        end
-
-      end
+    def modify_name(name)
+      return "#{name [-13..-10]}/#{name[-9..-8]}/#{name[-7..-6]} : #{name[-4..-1]}"
     end
+
+    def trim_name(name)
+      index = name.split("/log/").last.capitalize
+    end
+
   end
 end
